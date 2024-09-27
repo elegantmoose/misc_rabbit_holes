1663bd1cb0b54d543d9cdd83e176ed381198fb73
elegantmoose <mkouremetis16@gmail.com>
2024-09-26 23:50:03 -0400

    update and tweeks

--- minimax_tic_tac_toe/play_tic_tac_toe.py
+++ minimax_tic_tac_toe/play_tic_tac_toe.py
@@ -9,7 +9,8 @@ from os import stat
 import networkx as nx
 import copy
 from enum import Enum
-#import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 import time
 
 import logging
@@ -35,18 +36,18 @@ WIN_LINES = [
     (2, 4, 6),
     (2, 5, 8),
     (3, 4, 5),
-    (6, 7, 8)
+    (6, 7, 8),
 ]
 
 
 STATE_ID_COUNTER = 0
 
-logging.basicConfig(filename='minimax.log', level=logging.DEBUG)
+logging.basicConfig(filename="minimax.log", level=logging.DEBUG)
 
 
 class AdversaryAI(Enum):
-    MINIMAX = 'minimax'
-    MINIMAX_AB_PRUNE = 'minimax-ab-prune'
+    MINIMAX = "minimax"
+    MINIMAX_AB_PRUNE = "minimax-ab-prune"
 
 
 class TicTacToeState:
@@ -55,22 +56,29 @@ class TicTacToeState:
     A '0' is open
     A '1' is a user mark (0)
     A '2' in the place idx is an AI mark (X)
-   
+
     The board is laid out as:
-    
+
         0  3  6
         1  4  7
         2  5  8
     """
-    def __init__(self, depth=0, state=[0,0,0,0,0,0,0,0,0], utility=None):
+
+    def __init__(self, depth=0, state=[0, 0, 0, 0, 0, 0, 0, 0, 0], utility=None):
         self.id = self.create_state_id()
         self.depth = depth
         self.utility = utility
         self.state = state
         self.next_state = -1
-    
+
     def asdict(self):
-        return {'id': self.id, 'depth': self.depth, 'state': self.state, 'utility': self.utility, 'next_state': self.next_state}
+        return {
+            "id": self.id,
+            "depth": self.depth,
+            "state": self.state,
+            "utility": self.utility,
+            "next_state": self.next_state,
+        }
 
     def __str__(self):
         return str(self.asdict())
@@ -79,7 +87,7 @@ class TicTacToeState:
     def create_state_id():
         global STATE_ID_COUNTER
         id = str(STATE_ID_COUNTER)
-        STATE_ID_COUNTER += 1 
+        STATE_ID_COUNTER += 1
         return id
 
 
@@ -103,7 +111,7 @@ def expand_ttt_states(state, turn):
     """ """
     expanded_states = []
     for idx, i in enumerate(state.state):
-        if i == OPEN_SQUARE: # open square
+        if i == OPEN_SQUARE:  # open square
             new_state = TicTacToeState()
             new_state.state = copy.copy(state.state)
             new_state.state[idx] = turn
@@ -143,10 +151,12 @@ def max_value(state, sibling_state_utilities, graph, ab_prune=False):
             # utility score that will at least be better than the utility
             # found on this state recursion
             break
-    
+
         graph.add_node(next_state.id, **next_state.asdict())
         graph.add_edge(state.id, next_state.id)
-        sub_states_explored = min_value(next_state, expanded_sibling_state_utilities, graph, ab_prune=ab_prune)
+        sub_states_explored = min_value(
+            next_state, expanded_sibling_state_utilities, graph, ab_prune=ab_prune
+        )
         # logging.info("max---")
         # logging.info(f"state: {state}")
         # logging.info(f"next_state: {next_state}")
@@ -155,7 +165,7 @@ def max_value(state, sibling_state_utilities, graph, ab_prune=False):
             state.utility = next_state.utility
             state.next_state = next_state
         states_explored += sub_states_explored
-    #assert state.next_state != -1
+    # assert state.next_state != -1
     return states_explored
 
 
@@ -178,7 +188,9 @@ def min_value(state, sibling_state_utilities, graph, ab_prune=False):
         graph.add_node(next_state.id, **next_state.asdict())
         graph.add_edge(state.id, next_state.id)
 
-        sub_states_explored = max_value(next_state, expanded_sibling_state_utilities, graph, ab_prune=ab_prune)
+        sub_states_explored = max_value(
+            next_state, expanded_sibling_state_utilities, graph, ab_prune=ab_prune
+        )
         # logging.info("min---")
         # logging.info(f"state: {state}")
         # logging.info(f"next_state: {next_state}")
@@ -188,7 +200,7 @@ def min_value(state, sibling_state_utilities, graph, ab_prune=False):
             state.next_state = next_state
         expanded_sibling_state_utilities.add(next_state.utility)
         states_explored += sub_states_explored
-    #assert state.next_state != -1
+    # assert state.next_state != -1
     return states_explored
 
 
@@ -210,7 +222,10 @@ def alpha_beta_prune(state, parent_state_sibling_utilities, turn):
 def terminal_state(state):
     for line in WIN_LINES:
         if state.state[line[0]] != OPEN_SQUARE:
-            if state.state[line[0]] == state.state[line[1]] and state.state[line[1]] == state.state[line[2]]:
+            if (
+                state.state[line[0]] == state.state[line[1]]
+                and state.state[line[1]] == state.state[line[2]]
+            ):
                 return state.state[line[0]]
     if 0 not in set(state.state):
         return OPEN_SQUARE
@@ -223,14 +238,16 @@ def terminal_state(state):
 def next_ai_move(GAME_BOARD, ai=AdversaryAI.MINIMAX, stats=False):
     ab_prune = True if ai == AdversaryAI.MINIMAX_AB_PRUNE else False
     start_time = time.time()
-    next_state, states_explored, minimax_tree = minimax_search(GAME_BOARD, ab_prune=ab_prune)
+    next_state, states_explored, minimax_tree = minimax_search(
+        GAME_BOARD, ab_prune=ab_prune
+    )
     stop_time = time.time()
     search_stats = None
     if stats:
         search_stats = {
-            'time': (stop_time - start_time),
-            'states_explored': states_explored,
-            'tree': minimax_tree
+            "time": (stop_time - start_time),
+            "states_explored": states_explored,
+            "tree": minimax_tree,
         }
     return copy.copy(next_state.state), search_stats
 
@@ -241,9 +258,12 @@ def next_turn(turn):
 
 def prompt_and_apply_user_move(GAME_BOARD):
     print_board_layout()
-    print('Your move:')
+    print("Your move:")
     user_move = int(input())
-    assert user_move in [0,1,2,3,4,5,6,7,8,9] and GAME_BOARD[user_move] == OPEN_SQUARE
+    assert (
+        user_move in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        and GAME_BOARD[user_move] == OPEN_SQUARE
+    )
     GAME_BOARD[user_move] = USER_SQUARE
 
 
@@ -253,30 +273,33 @@ def prompt_and_apply_user_move(GAME_BOARD):
 def print_minimax_tree(graph):
     for node, neighbors in graph.adj.items():
         print(node)
-        print('----')
+        print("----")
         print(neighbors)
 
 
 def print_board_layout():
     print("--Board layout--")
-    print('   0  3  6')
-    print('   1  4  7')
-    print('   2  5  8')
-    print('--------------')
+    print("   0  3  6")
+    print("   1  4  7")
+    print("   2  5  8")
+    print("--------------")
 
 
 def show_winner(GAME_BOARD):
     for line in WIN_LINES:
         if GAME_BOARD[line[0]] != OPEN_SQUARE:
-            if GAME_BOARD[line[0]] == GAME_BOARD[line[1]] and GAME_BOARD[line[1]] == GAME_BOARD[line[2]]:
+            if (
+                GAME_BOARD[line[0]] == GAME_BOARD[line[1]]
+                and GAME_BOARD[line[1]] == GAME_BOARD[line[2]]
+            ):
                 print("Game Over")
                 if GAME_BOARD[line[0]] == USER_SQUARE:
-                    print('User wins.')
+                    print("User wins.")
                 else:
-                    print('AI wins.')
+                    print("AI wins.")
                 return True
     if not len([i for i in GAME_BOARD if i == 0]):
-        print('Game Over. Tie.')
+        print("Game Over. Tie.")
         return True
     return False
 
@@ -285,11 +308,11 @@ def print_board(GAME_BOARD):
     present = copy.copy(GAME_BOARD)
     for idx, i in enumerate(present):
         if i == 0:
-            present[idx] = ' '
+            present[idx] = " "
         elif i == 1:
-            present[idx] = 'O'
+            present[idx] = "O"
         else:
-            present[idx] = 'X'
+            present[idx] = "X"
 
     print("\n")
     print(f"{present[0]} | {present[3]} | {present[6]} ")
@@ -297,7 +320,7 @@ def print_board(GAME_BOARD):
     print(f"{present[1]} | {present[4]} | {present[7]} ")
     print(f"----------")
     print(f"{present[2]} | {present[5]} | {present[8]} ")
-    print('\n')
+    print("\n")
 
 
 """ Main """
@@ -306,12 +329,12 @@ def print_board(GAME_BOARD):
 def play_game(ai=AdversaryAI.MINIMAX, stats=False):
     """
     The board is laid out as:
-    
+
         0  3  6
         1  4  7
         2  5  8
     """
-    GAME_BOARD = [0,0,0,0,0,0,0,0,0]
+    GAME_BOARD = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     # start game
     while True:
@@ -322,7 +345,9 @@ def play_game(ai=AdversaryAI.MINIMAX, stats=False):
             break
         GAME_BOARD, search_stats = next_ai_move(GAME_BOARD, stats=stats, ai=ai)
         if stats:
-            print(f"Took AI {search_stats['time']} seconds and searched {search_stats['states_explored']} states.")
+            print(
+                f"Took AI {search_stats['time']} seconds and searched {search_stats['states_explored']} states."
+            )
         print_board(GAME_BOARD)
         if show_winner(GAME_BOARD):
             break
@@ -331,18 +356,18 @@ def play_game(ai=AdversaryAI.MINIMAX, stats=False):
 def _get_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--ai',
-        dest='ai',
+        "--ai",
+        dest="ai",
         default=AdversaryAI.MINIMAX,
-        help='type of AI adversary. Options include: "minimax", "mimimax-ab-prune'
+        help='type of AI adversary. Options include: "minimax", "mimimax-ab-prune',
     )
-    
+
     parser.add_argument(
-        '--stats',
-        dest='stats',
-        action='store_true',
+        "--stats",
+        dest="stats",
+        action="store_true",
         default=False,
-        help="verbose mode to print out AI adversarial game search stats"
+        help="verbose mode to print out AI adversarial game search stats",
     )
 
     return parser
@@ -355,5 +380,5 @@ def main():
     play_game(ai=args.ai, stats=args.stats)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- minimax_tic_tac_toe/tic_tac_toe_state_space.py
+++ minimax_tic_tac_toe/tic_tac_toe_state_space.py
@@ -8,6 +8,7 @@ will be produced.
 @author elegantmoose
 """
 
+import argparse
 import networkx as nx
 import copy
 import matplotlib.pyplot as plt
@@ -30,7 +31,7 @@ WIN_LINES = [
     (2, 4, 6),
     (2, 5, 8),
     (3, 4, 5),
-    (6, 7, 8)
+    (6, 7, 8),
 ]
 
 
@@ -43,23 +44,30 @@ class TicTacToeState:
     A '0' is open
     A '1' is a user mark (0)
     A '2' in the place idx is an AI mark (X)
-   
+
     The board is laid out as:
-    
+
         0  3  6
         1  4  7
         2  5  8
     """
+
     state_counter = 0
-    def __init__(self, depth=0, state=[0,0,0,0,0,0,0,0,0], utility=0):
+
+    def __init__(self, depth=0, state=[0, 0, 0, 0, 0, 0, 0, 0, 0], utility=0):
         global STATE_ID_COUNTER
         self.id = str(STATE_ID_COUNTER)
-        STATE_ID_COUNTER += 1 
+        STATE_ID_COUNTER += 1
         self.depth = depth
         self.utility = utility
-    
+
     def asdict(self):
-        return {'id': self.id, 'depth': self.depth, 'state': self.state, 'utility': self.utility}
+        return {
+            "id": self.id,
+            "depth": self.depth,
+            "state": self.state,
+            "utility": self.utility,
+        }
 
 
 def next_turn(turn):
@@ -68,14 +76,14 @@ def next_turn(turn):
 
 def _line_moves(line, state):
     state_squares = [state.state[line[0]], state.state[line[1]], state.state[line[2]]]
-    line_moves = {0: 0, 1:0, 2:0}
+    line_moves = {0: 0, 1: 0, 2: 0}
     for square in state_squares:
         line_moves[square] += 1
     return line_moves
 
 
 def eval_function(state):
-    """ 
+    """
     Eval = 3*X2 + X1 - (3*O2 + O1)
 
     where:
@@ -90,28 +98,28 @@ def eval_function(state):
     o_2 = 0
     for line in WIN_LINES:
         line_moves = _line_moves(line, state)
-        if line_moves == {0:0, 1:3, 2:0}:
+        if line_moves == {0: 0, 1: 3, 2: 0}:
             return USER_WIN
-        elif line_moves == {0:0, 1:0, 2:3}:
+        elif line_moves == {0: 0, 1: 0, 2: 3}:
             return AI_WIN
-        elif line_moves == {0:1, 1:2, 2:0}:
+        elif line_moves == {0: 1, 1: 2, 2: 0}:
             o_2 += 1
-        elif line_moves == {0:2, 1:1, 2:0}:
+        elif line_moves == {0: 2, 1: 1, 2: 0}:
             o_1 += 1
-        elif line_moves == {0:1, 1:0, 2:2}:
+        elif line_moves == {0: 1, 1: 0, 2: 2}:
             x_2 += 1
-        elif line_moves == {0:2, 1:0, 2:1}:
+        elif line_moves == {0: 2, 1: 0, 2: 1}:
             x_1 += 1
         else:
             pass
-    return 3*x_2 + x_1 - (3*o_2 + o_1)
+    return 3 * x_2 + x_1 - (3 * o_2 + o_1)
 
 
 def expand(state, turn):
-    #print(f"(EXPAND) State: {state.asdict()},  turn: {turn}")
+    # print(f"(EXPAND) State: {state.asdict()},  turn: {turn}")
     expanded_states = []
     for idx, i in enumerate(state.state):
-        if i == 0: # open square
+        if i == 0:  # open square
             new_state = TicTacToeState()
             new_state.state = copy.copy(state.state)
             new_state.state[idx] = turn
@@ -130,19 +138,64 @@ def search(graph, state, turn, stop_depth=None):
     for new_state in expanded_states:
         graph.add_node(new_state.id, **new_state.asdict())
         graph.add_edge(state.id, new_state.id)
-        substates = search(graph, new_state, next_turn(turn), stop_depth=stop_depth)
+
+        substates = search(
+            graph,
+            new_state,
+            next_turn(turn),
+            stop_depth=(stop_depth - 1) if stop_depth is not None else None,
+        )
         # add subtree max-min values
     print(f"(SEARCH) State: {state.asdict()}     Turn: {turn}")
-    print(f'---Expanded to:')
+    print(f"---Expanded to:")
     for s in expanded_states:
-        print(f'--- {s.asdict()}')
+        print(f"--- {s.asdict()}")
     return expanded_states
 
 
+def color_best_states(graph):
+    """ """
+    best = {}
+    running_best = 0
+    for node_id in graph.nodes():
+        state = graph.nodes[node_id]
+        if state["utility"] >= running_best:
+            if state["id"] not in best:
+                best[state["id"]] = [state["id"]]
+            else:
+                best[state["utility"]].append(state["id"])
+            running_best = state["utility"]
+
+    # removing all but highest value nodes
+    best = best[sorted(list(best.keys()), reverse=True)[0]]
+
+    node_color_map = []
+    for node_id in graph.nodes():
+
+        if node_id in best:
+            node_color_map.append("green")
+        else:
+            node_color_map.append("blue")
+
+    return node_color_map
+
+
+def _get_argparser():
+    """to organize and clean format argparser args"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--depth", default=None, help="search depth")
+
+    return parser
+
+
 def main():
+    parser = _get_argparser()
+    args = parser.parse_args()
+
     # initialize root state
     initial_state = TicTacToeState()
-    initial_state.state = [2,0,0,0,1,0,1,0,2]
+    initial_state.state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     initial_state.utility = eval_function(initial_state)
 
     # initialize graph with first node
@@ -150,11 +203,18 @@ def main():
     G.add_node(initial_state.id, **initial_state.asdict())
 
     # execute search
-    search(graph=G, state=initial_state, turn=AI_SQUARE)
-    
+    search(graph=G, state=initial_state, turn=AI_SQUARE, stop_depth=int(args.depth))
+
+    # best path
+    node_color_map = color_best_states(G)
+
     # -- Print search
     print(f"Graph node count (states): {len(list(G.nodes))}")
-    nx.draw_planar(G, with_labels=True)
+    print(f"Best states:")
+    for idx, color in enumerate(node_color_map):
+        if color == "green":
+            print(G.nodes[str(idx)])
+    nx.draw_networkx(G, with_labels=True, node_color=node_color_map)
     plt.show()
 
 
